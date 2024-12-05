e5a495ac892cebdb114c0d65699fdafccfe2c230
Frazer McIntosh <fgm@trustpilot.com>
2024-12-05 21:40:58 +0000

    day 3

--- 2024/src/day03/index.ts
+++ 2024/src/day03/index.ts
@@ -1,21 +1,49 @@
 import run from "aocrunner";
+import lodash from "lodash";
 
 const parseInput = (rawInput: string) => rawInput;
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  const regex = /mul\(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]),([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\)/g
+  const regex =
+    /mul\(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]),([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\)/g;
 
   const matches = input.matchAll(regex);
 
-  return matches[0];
+  const products = Array.from(
+    matches,
+    ([_, num1, num2]) => parseInt(num1) * parseInt(num2),
+  );
+
+  return lodash.sum(products);
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const regex =
+    /(mul\(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]),([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\))|do\(\)|don\'t\(\)/g;
+
+  const matches = input.matchAll(regex);
+
+  let enabled = true;
+  let sum = 0;
+
+  for (const [op, _, num1, num2] of matches) {
+    switch (op) {
+      case "do()":
+        enabled = true;
+        break;
+      case "don't()":
+        enabled = false;
+        break;
+      default:
+        sum += enabled ? parseInt(num1) * parseInt(num2) : 0;
+    }
+  }
+
+  return sum;
 };
 
 run({
